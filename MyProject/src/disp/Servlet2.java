51b8cae647be5c1b7f1480a1191cd795f504ef81
Umair_Git <umairabdullah786@gmail.com>
2020-06-23 19:28:32 +0530

    Third commit up to date at last 23 june.

--- MyProject/src/disp/Servlet2.java
+++ MyProject/src/disp/Servlet2.java
@@ -1,8 +1,8 @@
-package mypkg;
+package disp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class LoginServlet
+ * Servlet implementation class Servlet2
  */
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
+@WebServlet("/s2")
+public class Servlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LoginServlet() {
+    public Servlet2() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,27 +30,11 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			PrintWriter out=response.getWriter();
-			String email=request.getParameter("email");
-			String password=request.getParameter("pass");
-			MyDao md=MyDao.getObject();
-			Statement st=md.getConnection();
-			String query_getpass="SELECT status,password FROM login_table WHERE (username='"+email+"')";
-			ResultSet rs=st.executeQuery(query_getpass);
-			if(rs.next()) {
-				String passwordfromdb=rs.getString("password");
-				int stat=rs.getInt("status");
-				if(password.equals(passwordfromdb) && stat==1) {
-					out.println("Welcome "+email);
-				}else if(password.equals(passwordfromdb) && stat==0){
-					out.println("Email is not verifed!!");
-				}else {
-					out.println("Either email or password is wrong");
-				}
-			}else {
-				out.println("User does not exist");
-			}
-				
-		}catch(Exception e) {
+			out.println("Response from Servlet 2");
+			String na=request.getAttribute("name").toString();
+			out.println(na);
+			
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
